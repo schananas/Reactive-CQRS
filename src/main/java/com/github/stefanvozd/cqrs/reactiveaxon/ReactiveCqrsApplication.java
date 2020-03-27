f6f94522d3846d20e8335aef93c74fee46c57aa1
Stefan Dragisic <stefan.dragisic@axoniq.io>
2020-03-27 17:44:07 +0100

    First commit

---
+++ src/main/java/com/github/stefanvozd/cqrs/reactiveaxon/ReactiveCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.github.stefanvozd.cqrs.reactiveaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCqrsApplication.class, args);
+	}
+
+}
